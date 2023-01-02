491ddab6e18db16bbe05f3643d0ee7ce97ce6ddc
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-02 14:07:13 -0800

    refactored AppCont methods into AppHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+    
+    
+    # memoization
+    def current_user
+         @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        current_user
+    end
+    
+    
+    
     def current_path
         #use current path and check to see if it includes user params[:user_id] to render view descriptions accordingly. 
         current_uri = request.env['PATH_INFO']
