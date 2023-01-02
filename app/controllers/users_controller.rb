5cef5373ca04142312ce9f3b19ecfa6382d2c2f4
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-01 20:40:42 -0800

    users controller methods refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,30 @@
 class UsersController < ApplicationController
-  
+  before_action :current_user, only: [:show]
+
+
   def new
-    @user = User.new
-  end
+        if !current_user
+            @user = User.new
+        else 
+            redirect_to user_path(current_user)
+        end 
+     end 
+ 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            render 'new'
+        end 
+    end 
 
-  def create
-    @user = User.new(user_params)
-  end
-
-  def show
-    
-  end
+    def show
+        
+    end
 
   private 
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through
   def user_params
