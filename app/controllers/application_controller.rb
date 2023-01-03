92d7240c2ae69dcfcb68a9c30382be5b44992996
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-02 21:59:48 -0800

    spec updated app work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,22 +8,6 @@ class ApplicationController < ActionController::Base
         end
     end
 
-    # #find current user
-    # def current_user
-    #     @user = User.find_by(id: session[:user_id])
-    # end
-
-    # #verify login to make sure session user_id evaluates to true
-    # def logged_in?
-    #     !!session[:user_id]
-    # end
-
-    # def require_login
-    #     if !logged_in?  
-    #         flash[:alert] = "Please login to continue" 
-    #         redirect_to root_path
-    #     end
-    # end
   
 
 end
