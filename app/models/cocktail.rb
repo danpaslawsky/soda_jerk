781d60be6ac1ee3c5e59f8f09d6cfb1d8d8bfd76
Dan Paslawsky <danpaslawsky@gmail.com>
2023-01-02 21:59:00 -0800

    ccktail M -#queriesall cocktails for userindexview

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,14 @@ class Cocktail < ApplicationRecord
     belongs_to :user
     has_many :cocktail_ingredients
     has_many :ingredients, through: :cocktail_ingredients
+
+    
+    
+    def self.user_cocktails_index(user) #queries all cocktails for a  specific user for index view
+        @cocktails = user.cocktails.all
+    end 
+
+    
+
+
 end
